bdc1493c3da698cd1ce1ad315ec8123603351d2b
thomasmrt13 <75833671+thomasmrt13@users.noreply.github.com>
2022-03-29 22:10:34 +0400

    Add files via upload

---
+++ register.php
@@ -0,0 +1,35 @@
+<?php
+    /* Connection au server */
+
+    $co=mysqli_connect("localhost", "root", "");
+    $db=mysqli_select_db($co, "<DATABASE_NAME>");
+
+    /* Itercept les requetes post */
+
+    $content = file_get_contents('php://input');
+    $decode = json_decode($content, true);
+
+    /* Assigner les valeurs de la requête aux variables */
+
+    $user_email = $decode['Email'];
+    $user_password = $decode['Password'];
+
+    /* Requête sql pour inserer un nouvel utilisateur dans la table */
+
+    $query = "insert into <TAB_NAME>(Email, Password) values('$user_email', '$user_password')";
+
+    /* Récuprer les  infos de la requête*/
+    
+    $request = mysqli_query($co, $query);
+    
+    if ($request)
+    {
+        $Message = "User registered";
+    }
+    else
+    {
+        $Message = "Server Error";
+    }
+    $response[] = array("Message"=>$Message);
+    echo json_encode($response);
+?>
